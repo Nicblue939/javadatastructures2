8e0d32b4b2c22806d8840034770a9c43d45fc6f6
Nicblue939 <73443833+Nicblue939@users.noreply.github.com>
2020-11-24 10:55:57 -0500

    Add files via upload

---
+++ Java Data Stsructures 2/src/jdstructurespackage/jdstructuresjava.java
@@ -0,0 +1,40 @@
+package jdstructurespackage;
+import java.util.*;
+
+public class jdstructuresjava {
+	
+
+
+	public static void main(String[] args) {
+		 
+	//	 int[] array = {1,5,6,5,4,1};
+    //   int sum = 0;
+		 
+	//	 for(int i = 0; i<array.length; i++) {
+	//		 sum = sum + array[i];
+	//	 }
+	//	 System.out.println(sum);
+		
+
+		
+		int size = 4;
+		int power = 2;
+		
+		int myArray []; 
+		myArray = new int [size];
+		
+		for (int i=0; i<myArray.length; i++) {
+			
+		double j = (power);
+		
+		System.out.println(Math.pow(i,j));
+		}
+		}
+	}
+
+		
+			
+			
+	
+
+
